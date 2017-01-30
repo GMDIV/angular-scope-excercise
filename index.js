c82ad6490178d8a2d8d11a33268bc5d5f579cd19
Mitchell Deane <mitchelld20@yahoo.com>
2017-01-30 17:54:12 -0600

    Added a rootScope, changed reset button to rootScope, and added comments.

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ var app = angular.module('scope-practice', []);
 //However, if you change the input in the ChildController, it will of course not affect the ParentController. 
 //But, later changing the input in the ParentController will no longer affect the ChildController.
 //They have the same variable names, so once the child is given a different name, it no longer looks up to the parent. 
+//Think of it like Object.create().
 
 
-app.controller('ParentController', function ($scope) {
-	$scope.name = "Parent";
+app.controller('ParentController', function ($scope, $rootScope) {
+	// $scope.name = "ParentController Default";
 
-	$scope.reset = function () { //Resets the name of the parent controller's scope.name
-		$scope.name = "Parent";
-	};//location - inside child controller area
+	// //Resets the name of the parent controller's scope.name
+	// $scope.reset = function () { 
+	// 	$scope.name = "ParentController has been reset";
+	// };
+	// //location - inside child controller area
 
 });
 
-app.controller('ChildController', function ($scope) {
+app.controller('ChildController', function ($scope, $rootScope) {
+
+	//This function make makes the button in ChildController set to rootScope as opposed to ParentController
+	//When this is clicked, it changes every input entry globally. Though still, if the ChildController or 
+	//ParentController's entry was changed, then they will have variables of the same name as the rootScope and not change.
+	$scope.reset = function () { 
+		$rootScope.name = "Reset by ChildController";
+	};
 });
 
 
