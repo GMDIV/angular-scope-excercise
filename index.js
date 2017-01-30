4727683a10df851ff96d7f71606965a424b6247f
Mitchell Deane <mitchelld20@yahoo.com>
2017-01-30 16:41:52 -0600

    added comments to describe what is going on with the code. Added an input field for the ChildController to show databinding scope issues.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var app = angular.module('scope-practice', []);
 
+
+//From the start, the ParentController div and ChildControler div within it will have the scope name of Parent.
+//If you add change anything within the ParentController's input, it will display it on the parent and child div.
+//However, if you change the input in the ChildController, it will of course not affect the ParentController. 
+//But, later changing the input in the ParentController will no longer affect the ChildController.
+//They have the same variable names, so once the child is given a different name, it no longer looks up to the parent. 
+
+
 app.controller('ParentController', function ($scope) {
 	$scope.name = "Parent";
 
